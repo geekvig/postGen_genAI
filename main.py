a4acdc39ae8e49bd8692fe4cc29e93ff16965af0
geekvig <anandpadman91@gmail.com>
2025-04-24 15:49:29 -0700

    commiting code changes to this project

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import json
-
-def process_posts(raw_file_path, processed_file_path="data/processed_posts.json"):
-    with open(raw_file_path, encoding='utf-8') as file:
-        posts = json.load(file)
-        print (posts)
+import streamlit as st
+from fewshot import FewShotPosts
+from post_generator import generate_post
 
 
+# Options for length and language
+length_options = ["Short", "Medium", "Long"]
+language_options = ["English", "Hinglish"]
+
+
+# Main app layout
+def main():
+    st.subheader("LinkedIn Post Generator: Codebasics")
+
+    # Create three columns for the dropdowns
+    col1, col2, col3 = st.columns(3)
+
+    fs = FewShotPosts()
+    tags = fs.get_tags()
+    with col1:
+        # Dropdown for Topic (Tags)
+        selected_tag = st.selectbox("Topic", options=tags)
+
+    with col2:
+        # Dropdown for Length
+        selected_length = st.selectbox("Length", options=length_options)
+
+    with col3:
+        # Dropdown for Language
+        selected_language = st.selectbox("Language", options=language_options)
 
 
 
+    # Generate Button
+    if st.button("Generate"):
+        post = generate_post(selected_length, selected_language, selected_tag)
+        st.write(post)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    process_posts()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Run the app
+if __name__ == "__main__":
+    main()
